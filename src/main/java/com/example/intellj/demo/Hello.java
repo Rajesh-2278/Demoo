0a9e2c7f0d1fb682f384a078cc227a75eb0cfe45
Rajesh-2278 <134488582+Rajesh-2278@users.noreply.github.com>
2025-03-07 10:36:53 +0530

    Update Hello.java

--- src/main/java/com/example/intellj/demo/Hello.java
+++ src/main/java/com/example/intellj/demo/Hello.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Hello {
     @GetMapping("/na")
     public String run(){
-
+//Rajesh naik
         return "hellooo";
     }
 }
